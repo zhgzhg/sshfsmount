20c533027be5723f6b09ab95f7298b11a02d9629
zhgzhg <perkoz@mail.bg>
2019-05-27 15:39:07 +0300

    Add double brackets to meet convention

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# (C) 30.01.2015 zhgzhg
+# (C) 27.05.2019 zhgzhg
 # installation script for sshfsmount and sshfsunmount tools
 
-if [ "$(id -u)" != "0" ]; then
+if [[ "$(id -u)" != "0" ]]; then
       echo [You need to run this script as root!]
       exit 1
 fi
@@ -11,7 +11,7 @@ echo -e "\nInstalling sshfsmount project...\n"
 
 echo Copying sshfsmount to /usr/local/bin
 cp -f sshfsmount.sh /usr/local/bin/sshfsmount
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   echo Success!
   chmod 755 /usr/local/bin/sshfsmount
 else
@@ -22,7 +22,7 @@ SUCCESS=0
 
 echo Copying sshfsunmount to /usr/local/bin
 cp -f sshfsunmount.sh /usr/local/bin/sshfsunmount
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   echo Success!
   chmod 755 /usr/local/bin/sshfsunmount
   SUCCESS=1
@@ -32,7 +32,7 @@ fi
 
 echo -e "\nDone!\n"
 
-if [ $SUCCESS -eq 1 ]; then
+if [[ ${SUCCESS} -eq 1 ]]; then
   echo Now you can use directly these tools as a commands named
   echo 'sshfsmount' and 'sshfsunmount'.
 fi
